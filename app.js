647ffe72fa45bd273f310e7d5ec90815cf76eb29
infotest123 <info.trushna.ninawe@gmail.com>
2026-09-05 14:52:11 +0530

    updated app.js and package.json

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
-const http = require("http");
+// const http = require("http");
 
-const PORT = 3000;
+// const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        "Content-Type": "text/html"
-    });
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, {
+//         "Content-Type": "text/html"
+//     });
 
-    res.end(`
-        <h1>Hello from Node.js team this is cicd to automate application deployment</h1>
-        <p>Application deployed using GitHub Actions → AWS EC2</p>
-    `);
+//     res.end(`
+//         <h1>Hello from Node.js team this is cicd to automate application deployment</h1>
+//         <p>Application deployed using GitHub Actions → AWS EC2</p>
+//     `);
+// });
+
+// server.listen(PORT, "0.0.0.0", () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
+
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const ENV = process.env.APP_ENV || "DEV";
+
+app.get("/", (req, res) => {
+  res.send(`Hello Team - Environment: ${ENV}`);
 });
 
-server.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on port ${PORT}`);
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
 });
+
+app.listen(PORT, () => {
+  console.log(`Application running on port ${PORT}`);
+});
+
